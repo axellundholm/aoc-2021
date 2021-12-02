9c423451837752858c6e4acef2daeff518485ad0
axellundholm <axel.lundholm@me.com>
2021-12-02 18:49:18 +0100

    complete solution for day one

--- day-1.py
+++ day-1.py
@@ -9,6 +9,15 @@ def count(measurements):
 
 	return counter
 
+def filter_list(measurements):
+
+	filtered_measurements = []
+
+	for i in range(len(measurements) - 1):
+		filtered_measurements.append(measurements[i - 1] + measurements[i] + measurements[i + 1])
+	
+	return filtered_measurements
+
 
 if __name__=="__main__":
 	f = open('input-1.txt', 'r')
@@ -17,3 +26,4 @@ if __name__=="__main__":
 	l = [int(x.strip()) for x in l]
 
 	print("Answer to Part One: " + str(count(l)))
+	print("Answer to Part Two: " + str(count(filter_list(l))))
