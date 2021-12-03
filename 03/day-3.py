f9b05042705a1f9f6d9715edfa84b17b464e9a30
axellundholm <axel.lundholm@me.com>
2021-12-03 09:12:00 +0100

    added solution for day three part one

--- 03/day-3.py
+++ 03/day-3.py
@@ -1,8 +1,32 @@
+import numpy as np
+
+
+def find_gd(list):
+
+	bin_sum = [0] * len(list[0])
+	gamma = []
+	delta = []
+
+	for l in list:
+		bin_sum = [int(a) + b for a, b in zip(l, bin_sum)]
+
+	for b in bin_sum:
+		gamma.append(str(1)) if b > len(list) / 2 else gamma.append(str(0))
+		delta.append(str(0)) if b > len(list) / 2 else delta.append(str(1))
+
+	return(int(''.join(gamma), 2) * int(''.join(delta), 2))
 
 
 
+	pass
 if __name__=="__main__":
 	f = open('input', 'r')
 	
 	l = f.readlines()
-	l = [int(x.strip()) for x in l]
+	l = [list(x.strip()) for x in l]
+
+	print(find_gd(l))
+
+	print("Answer to Part One: " + str(find_gd(l)))
+	print("Answer to Part Two: " + str('N/A'))
+	
