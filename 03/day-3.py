dccfb10ee471b4b276a872003a5b0b2cebddc519
axellundholm <axel.lundholm@me.com>
2021-12-05 02:12:45 +0100

    complete solution for day three

--- 03/day-3.py
+++ 03/day-3.py
@@ -1,32 +1,58 @@
-import numpy as np
 
-
-def find_gd(list):
-
+def significant_value(list):
+	
 	bin_sum = [0] * len(list[0])
-	gamma = []
-	delta = []
 
 	for l in list:
 		bin_sum = [int(a) + b for a, b in zip(l, bin_sum)]
 
+	return bin_sum
+
+
+def power_consumption(list):
+
+	gamma = []
+	epsilon = []
+	bin_sum = significant_value(list)
+	
 	for b in bin_sum:
 		gamma.append(str(1)) if b > len(list) / 2 else gamma.append(str(0))
-		delta.append(str(0)) if b > len(list) / 2 else delta.append(str(1))
+		epsilon.append(str(0)) if b > len(list) / 2 else epsilon.append(str(1))
 
-	return(int(''.join(gamma), 2) * int(''.join(delta), 2))
+	return(int(''.join(gamma), 2) * int(''.join(epsilon), 2))
 
 
+def oxygen_generator(list):
+	
+	for c in range(len(list[0])):
+		bin_sum = significant_value(list)
 
-	pass
+		list = [x for x in list if int(x[c]) is int(bin_sum[c] >= (float(len(list)) / 2))] 
+		
+		if len(list) == 1:
+			return int(''.join(list[0]), 2)
+
+def co2_scrubber(list):
+	
+	for c in range(len(list[0])):
+		bin_sum = significant_value(list)
+
+
+		list = [x for x in list if int(x[c]) is not int(bin_sum[c] >= (float(len(list)) / 2))] 
+		
+		if len(list) == 1:
+			return int(''.join(list[0]), 2)
+
+def life_support(list):
+
+	return oxygen_generator(list) * co2_scrubber(list)
+
+
 if __name__=="__main__":
 	f = open('input', 'r')
 	
 	l = f.readlines()
 	l = [list(x.strip()) for x in l]
 
-	print(find_gd(l))
-
-	print("Answer to Part One: " + str(find_gd(l)))
-	print("Answer to Part Two: " + str('N/A'))
-	
+	print("Answer to Part One: " + str(power_consumption(l)))
+	print("Answer to Part Two: " + str(life_support(l)))
